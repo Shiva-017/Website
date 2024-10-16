c2962d457023689ad0c782121e4d3da9c903dafe
Shiva Teja <dasi.s@northeastern.edu>
2024-10-16 13:56:59 -0400

    Updates

---
+++ client/assets/config-DWb3-BWr.js
@@ -0,0 +1,5 @@
+const i="Shiva Teja Dasi",s="Software Developer",n=["Designer","DevOps Engineer","Prototyper","Programmer","Nature Enthusiast"],o="https://hamishw.com",t="shiva-teja-dasi",e="@shiva017",a="Shiva-017",c="https://github.com/HamishMW/portfolio",r=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,h={name:i,role:s,disciplines:n,url:o,linkedin:t,figma:e,github:a,repo:c,ascii:r};export{h as c};
