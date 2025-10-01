f90cbcf47fcce2227d82d8cd1ebf1b54fae8b410
shiva-017 <dasi.s@northeastern.edu>
2025-10-01 12:00:00 -0400

    Refactor throttle to use Date.now() instead of new Date()

--- app/utils/throttle.js
+++ app/utils/throttle.js
@@ -2,7 +2,7 @@ export function throttle(func, timeFrame) {
   let lastTime = 0;
 
   return function (...args) {
-    const now = new Date();
+    const now = Date.now();
 
     if (now - lastTime >= timeFrame) {
       func(...args);
