da788268f0244cfa0168581c668f9478ed0900e0
shiva-017 <dasi.s@northeastern.edu>
2025-10-18 12:00:00 -0400

    Reduce discipline transition enter/exit times

--- app/routes/home/intro.jsx
+++ app/routes/home/intro.jsx
@@ -91,7 +91,7 @@ export function Intro({ id, sectionRef, scrollIndicatorHidden, ...rest }) {
                     <Transition
                       unmount
                       in={item === currentDiscipline}
-                      timeout={{ enter: 3000, exit: 2000 }}
+                      timeout={{ enter: 2500, exit: 1500 }}
                       key={item}
                     >
                       {({ status, nodeRef }) => (
